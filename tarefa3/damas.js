31a7d261db3820d7926919b514f7875dab7b461a
João Pedro Garcia Pereira <garcia.jops@gmail.com>
2022-09-23 21:22:04 -0300

    Movimento das pecas

--- tarefa3/damas.js
+++ tarefa3/damas.js
@@ -1,5 +1,6 @@
 const tamanhoCelula = 40;
 let pecaId = 0;
+let aux=false;
 document.body.append(criaTabuleiro());
 
 function criaTabuleiro() {
@@ -21,8 +22,12 @@ function criaTabuleiro() {
             celula.style.height = `${tamanhoCelula}px`;
             if (i % 2 == j % 2) {
                 celula.style.backgroundColor = 'black';
+                celula.addEventListener('dragover',allowDrop);
+                celula.addEventListener('drop',drop);
                 if (i * 8 + j <= 24) {
-                    celula.append(criaPeca('black'));
+                    const imagem = criaPeca('black')
+                    celula.append(imagem);
+                    imagem.setAttribute('draggable','false');
                 } else if (i * 8 + j >= 40) {
                     celula.append(criaPeca('red'));
                 }
@@ -33,27 +38,65 @@ function criaTabuleiro() {
     };
     return tabela;
 }
-
+var elementosBlack = document.getElementsByClassName('black');
+var elementosRed = document.getElementsByClassName('red');
+var h1 = document.getElementsByTagName("span");
 function criaPeca(cor) {
     let imagem = document.createElement('img');
     imagem.setAttribute('src', `img/${cor}.png`);
     imagem.setAttribute('width', `${tamanhoCelula-4}px`);
     imagem.setAttribute('height', `${tamanhoCelula-4}px`);
+    imagem.id='pecaId'+pecaId++;
+    imagem.className=''+cor;
+    imagem.addEventListener('dragstart',drag);
+    //imagem.addEventListener('drag',drag);
+    //imagem.addEventListener('dragend',drop);
     return imagem;
 }
-const imagem=document.querySelectorAll('img');
-imagem.forEach(img=>{
-    imagem.addEventListener('dragstart',dragstart);
-    imagem.addEventListener('drag',drag);
-    imagem.addEventListener('dragend',dragend);
-})
+// const imagem=document.querySelectorAll('img');
+//     imagem.forEach(img=>{
+//     imagem.addEventListener('dragstart',dragstart);
+//     imagem.addEventListener('drag',drag);
+//     imagem.addEventListener('dragend',dragend);
+// })
+function allowDrop(ev) {
+    ev.preventDefault();
+}
 
 function dragstart(){
     console.log('> Start Dragging')
 }
-function drag(){
-    console.log('> Dragging')
+
+function drag(ev){
+    //console.log(ev.target.id);
+    ev.dataTransfer.setData("pecaId", ev.target.id);
+    console.log(ev.dataTransfer.getData("pecaId"));
 }
+
 function dragend(){
     console.log('> Stoped Dragging')
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("pecaId");
+    ev.target.appendChild(document.getElementById(data));
+
+    //Alterando Jogadores
+    for(var i = 0; i < elementosRed.length; i++) {
+        elementosRed[i].setAttribute('draggable',''+aux);
+    }
+    aux=!aux;
+    for(var i = 0; i < elementosBlack.length; i++) {
+        elementosBlack[i].setAttribute('draggable',''+aux);
+    }
+    // if (aux==true){
+        
+    //     console.log(h1[0]);
+    //     console.log(h1);
+    //     h1.innerText="Preto";
+    //     //h1[0]='<span id="turno" style="color: black;">Preto</span>';
+    //     //h1[0].setAttribute('outerHTML',"'<span id=\"turno\" style=\"color: red;\">Vermelho</span>'");
+    //     //h1.innerText="Preto";
+    //  }
 }
